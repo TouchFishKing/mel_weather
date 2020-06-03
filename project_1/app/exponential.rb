2bce14cf08bed7c9f2038205f91f1d7a449a95bd
Lizhenan Li <lizhenanl54@gmail.com>
2020-06-03 16:44:39 +1000

    exponential regression finished

--- project_1/app/exponential.rb
+++ project_1/app/exponential.rb
@@ -1,3 +1,19 @@
 def exponential(time, data)
   # TODO
+  sum_of_x2y = 0
+  sum_of_ylny = 0
+  sum_of_xy = 0
+  sum_of_xylny = 0
+  sum_of_y = 0
+  n = 0
+  time.each do |x|
+    sum_of_x2y += x * x * data[n]
+    sum_of_ylny += data[n] * Math.log(data[n])
+    sum_of_xy += x * data[n]
+    sum_of_xylny += x * data[n] * Math.log(data[n])
+    sum_of_y += data[n]
+  end
+  a = (sum_of_x2y * sum_of_ylny - sum_of_xy * sum_of_xylny) / (sum_of_y * sum_of_x2y - sum_of_xy * sum_of_xy)
+  b = (sum_of_y * sum_of_xylny - sum_of_xy * sum_of_ylny) / (sum_of_y * sum_of_x2y - sum_of_xy * sum_of_xy)
+  "#{Math.exp(a)}e^#{b}x"
 end
