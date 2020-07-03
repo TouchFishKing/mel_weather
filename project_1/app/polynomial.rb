8fa8ffd3375a83632753cc6398ab74d651e30723
Lizhenan Li <lizhenanl54@gmail.com>
2020-07-03 20:49:55 +1000

    need to find the best fit equation for polynomial (degree of regression), the idea is to iterate over given degree range(2..10)and compare the goodness of fit

--- project_1/app/polynomial.rb
+++ project_1/app/polynomial.rb
@@ -1,12 +1,13 @@
 require 'matrix'
 
 def polynomial(time, data)
-  x_data = time.map { |x_i| (0..2).map { |pow| (x_i**pow).to_f } }
+  x_data = time.map { |x_i| (0..4).map { |pow| (x_i**pow).to_f } }
   mx = Matrix[*x_data]
   my = Matrix.column_vector(data)
   coefficients = ((mx.t * mx).inv * mx.t * my).transpose.to_a[0]
   equation = ''
-  coefficients.each_with_index do |coefficient, index|
+  coefficients.to_enum.with_index.reverse_each do |coefficient, index|
     equation += "#{coefficient.round(2)}x^#{index} + "
   end
+  equation.gsub('x^0 +', '')
 end
