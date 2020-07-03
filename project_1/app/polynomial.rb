834adcf990fc68b0cf672d35245f78a7c5fa0928
Lizhenan Li <lizhenanl54@gmail.com>
2020-07-03 19:57:18 +1000

    wtf polynomial

--- project_1/app/polynomial.rb
+++ project_1/app/polynomial.rb
@@ -1,5 +1,12 @@
 require 'matrix'
 
-def logarithmic(time, data)
-
+def polynomial(time, data)
+  x_data = time.map { |x_i| (0..2).map { |pow| (x_i**pow).to_f } }
+  mx = Matrix[*x_data]
+  my = Matrix.column_vector(data)
+  coefficients = ((mx.t * mx).inv * mx.t * my).transpose.to_a[0]
+  equation = ''
+  coefficients.each_with_index do |coefficient, index|
+    equation += "#{coefficient.round(2)}x^#{index} + "
+  end
 end
